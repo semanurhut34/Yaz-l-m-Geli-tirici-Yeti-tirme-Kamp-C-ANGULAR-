ccdff1ab30a97dfafb0de4dbfbc08702dbd77863
Semanur Hut <75675962+semanurhut34@users.noreply.github.com>
2022-08-27 19:45:27 +0300

    Delete day5Work4(InterfaceAbstractDemo) directory

--- day5Work4(InterfaceAbstractDemo)/Abstract/ICustomerCheckService.cs
+++
@@ -1,9 +0,0 @@
-﻿using day5Work4_InterfaceAbstractDemo_.Entities;
-
-namespace day5Work4_InterfaceAbstractDemo_.Concrete
-{
-    internal interface ICustomerCheckService
-    {
-        bool CheckIfRealPerson(Customer customer);
-    }
-}
--- day5Work4(InterfaceAbstractDemo)/Abstract/IEntitiy.cs
+++
@@ -1,9 +0,0 @@
-﻿using day5Work4_InterfaceAbstractDemo_.Entities;
-
-namespace day5Work4_InterfaceAbstractDemo_.Abstract
-{
-    internal interface IEntitiy
-    {
-        void Save(Customer customer);
-    }
-}
